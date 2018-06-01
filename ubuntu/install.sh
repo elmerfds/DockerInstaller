b5f765790f6e9726369fda7e7b32c517cd5602cd
elmerfdz <elmer.fdz@outlook.com>
2018-06-01 12:53:42 +0100

    fixes

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 #Docker Installer
 #author: elmerfdz
-version=v0.31.8
+version=v0.31.9
 
 #Script Requirements
 prereqname=('Curl' )
@@ -13,6 +13,7 @@ container_name=('Portainer' 'Watchtower' 'PHPmyadmin' 'Mariadb' 'Organizr' 'Post
 #Script config variables
 tzone=$(cat /etc/timezone)
 uid=$(id -u $(logname))
+user_home_dir="/home/$(logname)"
 ugp=$(cut -d: -f3 < <(getent group docker))
 ubu_code=$(cut -d: -f2 < <(lsb_release -c)| xargs)
 docker_dir='/opt/docker'
@@ -105,10 +106,10 @@ docker_install()
 docker_env_set()
 	{
         echo -e "\e[1;36m> Setting Docker environment variables...\e[0m"
-        echo 'PUID=$uid' >> /etc/environment
-        echo 'PGID=$ugp' >> /etc/environment
+        echo "PUID=$uid" >> /etc/environment
+        echo "PGID=$ugp" >> /etc/environment
         echo 'TZ="$tzone"' >> /etc/environment
-        echo 'USERDIR=/home/$(logname)"' >> /etc/environment
+        echo 'USERDIR="$user_home_dir"' >> /etc/environment
         echo 'ROOTDIR="/opt/docker"' >> /etc/environment
         echo 'DATADIR="/opt/docker/data"' >> /etc/environment
         echo 'MYSQL_ROOT_PASSWORD="changeMe!"' >> /etc/environment
